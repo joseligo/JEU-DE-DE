986fb8207291325604a36ebf3b6a6fc8b68d4894
Josephine <jostrezyk@hotmail.fr>
2024-05-20 10:48:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+let currentPlayer = 0; //défini le player 1 comme celui qui débute
+let currentScores = document.querySelectorAll(".currentScore"); //récupérer les scores courants dans un tableau
+
+let roll = document.querySelector("#roll"); //pour lancer le dé
+let hold = document.querySelector("#hold"); //pour finir son tour et récupérer les points
+let lancer = document.getElementById("lancer"); //pour changer l'image du dé
+
+function entierAleatoire(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function lancerDe() {
+  nb = entierAleatoire(1, 6);
+  lancer.innerHTML = `<i class="bi bi-dice-${nb} dice">`;
+  if (nb == 1) {
+    changePlayer();
+  } else {
+    currentScores[currentPlayer].innerHTML =
+      parseInt(currentScores[currentPlayer].innerHTML) + nb;
+  }
+}
+roll.addEventListener("click", lancerDe);
+
+let scoresTotaux = document.querySelectorAll(".totalScore");
+
+function finRound() {
+  scoresTotaux[currentPlayer].innerHTML =
+    parseInt(scoresTotaux[currentPlayer].innerHTML) +
+    parseInt(currentScores[currentPlayer].innerHTML);
+  if (parseInt(scoresTotaux[currentPlayer].innerHTML) >= 10) {
+    playerWin();
+  } else {
+    changePlayer();
+  }
+}
+hold.addEventListener("click", finRound);
+
+function changePlayer() {
+  currentScores[currentPlayer].innerHTML = 0;
+  let activ = document.getElementById(`dot${currentPlayer}`);
+  activ.classList.remove("active");
+  currentPlayer == 1 ? (currentPlayer = 0) : (currentPlayer = 1);
+  activ = document.getElementById(`dot${currentPlayer}`);
+  activ.classList.add("active");
+}
+
+function playerWin() {
+  currentScores[currentPlayer].innerHTML = 0;
+  alert(`Le joueur ${currentPlayer + 1} a gagné`);
+  roll.removeEventListener("click", lancerDe);
+  hold.removeEventListener("click", finRound);
+}
+
+btnReset = document.querySelector('.reset');
+btnReset.addEventListener('click', reset);
+
+function reset() {
+  currentScores.forEach(element => element.innerHTML = 0);
+  scoresTotaux.forEach(element => element.innerHTML = 0);
+  currentPlayer = 0;
+  roll.addEventListener("click", lancerDe);
+  hold.addEventListener("click", finRound);
+}
